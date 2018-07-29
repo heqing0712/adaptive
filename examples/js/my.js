61a5fcf49baf3580f925620a2969d9e74ef88450
heqing0712 <heqing0712@126.com>
2018-07-29 22:36:05 +0800

    update examples

--- examples/js/my.js
+++ examples/js/my.js
@@ -1,17 +1,46 @@
-﻿
+﻿var fl = {};
+
+/*
+* 图片懒加载
+*/
+fl.lzImg = function () {
+
+    var attr = "original",
+        $imgs = $("img[data-" + attr + "]"),
+        length = $imgs.length;
+
+    if (!length || !$.fn.lazyload) return;
+
+    $imgs.lazyload({
+
+        //effect: "fadeIn",
+        //effect_speed: 200,
+        data_attribute: attr,
+
+        //连续判断所有图片 是否在可视区 
+        failure_limit: length,
+
+        //700高度前 提前加载
+        threshold: 700,
+
+        //加载完毕移除源路径
+        load: function () {
+
+            $(this).removeAttr("data-" + attr);
+        }
+    });
+
+
+};
+
 
  /*
  * 初始化
  */
 fl.init = function () {
 
-     //更新价格
-     //fl.updatePrice($(".cn .num"));
+  
 
-     //转换为移动端的链接
-     fl.toMbUrl();
-
-     //图片懒加载
      fl.lzImg();
 
  };
